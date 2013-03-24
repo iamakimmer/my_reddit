ed57b4320a5f2dcf5c6e593ad53eb03e9ccb06c1
matthew kim <matthewjkim@gmail.com>
2013-03-24 19:20:38 +0000

    adding new user/link and relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324184824) do
+ActiveRecord::Schema.define(:version => 20130324190828) do
+
+  create_table "links", :force => true do |t|
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "title"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
